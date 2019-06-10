118c429d99a6d2481ee15fb020fe31a953975af7
Nir Abel <abel@post.bgu.ac.il>
2019-06-10 20:59:37 +0300

    Update fbConfig.js
    without the link to my firebase.

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyBsG9thqf31uT_VsnrMIHZ4iF_Qnan_LBQ",
-    authDomain: "doron-s-gym.firebaseapp.com",
-    databaseURL: "https://doron-s-gym.firebaseio.com",
-    projectId: "doron-s-gym",
-    storageBucket: "doron-s-gym.appspot.com",
-    messagingSenderId: "394882180672",
-    appId: "1:394882180672:web:37a78ea3c8f233c2"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
